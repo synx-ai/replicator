0fa145c6789a471bbd4070523376927d0e7e0ce7
Carlos Rivera <carlos@synx.co>
2020-01-09 16:16:39 -0600

    initial commit

---
+++ docs/precache-manifest.0d072786578eb03bdba764038795007f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d9788ed6eab517b6e613fb605400d89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1754ea18e3aacebef643",
+    "url": "/static/css/main.a99c28ba.chunk.css"
+  },
+  {
+    "revision": "546442459c1427516be1",
+    "url": "/static/js/2.383d45a9.chunk.js"
+  },
+  {
+    "revision": "1754ea18e3aacebef643",
+    "url": "/static/js/main.18465350.chunk.js"
+  },
+  {
+    "revision": "55984bab23f77f8af59d",
+    "url": "/static/js/runtime-main.2ab1de55.js"
+  }
+]);

9d9ee78d1f11a43fc64a41f9cf22e01583d1f265
Carlos Rivera <carlos@synx.co>
2020-01-09 16:27:24 -0600

    fixed relative path

---
+++ docs/precache-manifest.7160844d1ddcdff0b71f8f1b452abd04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1977c224e181ca75262e7d778abfc326",
+    "url": "/replicator/index.html"
+  },
+  {
+    "revision": "b4397e9d1d4dd2cd8f6a",
+    "url": "/replicator/static/css/main.a99c28ba.chunk.css"
+  },
+  {
+    "revision": "546442459c1427516be1",
+    "url": "/replicator/static/js/2.383d45a9.chunk.js"
+  },
+  {
+    "revision": "b4397e9d1d4dd2cd8f6a",
+    "url": "/replicator/static/js/main.54e71184.chunk.js"
+  },
+  {
+    "revision": "567131803a6af29269f4",
+    "url": "/replicator/static/js/runtime-main.c969a121.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d072786578eb03bdba764038795007f.js"
+  "/replicator/precache-manifest.7160844d1ddcdff0b71f8f1b452abd04.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/replicator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
